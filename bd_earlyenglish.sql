d6111cbf3e413b1a9f23b658f19cc4f0111031f2
LIVIA ALVES ESCOBAR <02349679004@senacrs.edu.br>
2024-07-01 10:57:54 -0300

    terceiro

--- bd_earlyenglish.sql
+++ bd_earlyenglish.sql
@@ -6,13 +6,8 @@ create table blog(
 	id int auto_increment primary key,
     titulo VARCHAR(100) not null,
     autor VARCHAR(45) not null,
-    conteudo VARCHAR(255) not null
+    conteudo text,
+    created_ate TIMESTAMP DEFAULT current_timestamp
 );
-
-alter table blog 
-add column (created_ate TIMESTAMP DEFAULT current_timestamp);
-
-delete from blog
-where id = "21";
 
 select * from blog;
