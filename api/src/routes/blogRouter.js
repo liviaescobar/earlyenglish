1b1014abe986a04e4228036475bfef3978219c51
Lívia Escobar <liviaescobar06@gmail.com>
2024-10-08 11:48:15 -0300

    swagger

--- api/src/routes/blogRouter.js
+++ api/src/routes/blogRouter.js
@@ -7,19 +7,79 @@ const { Router } = require('express');
 const router = Router();
 
 // Importa as funções de controle para gerenciar as rotas de blogs
-const { storeBlog, getBlog, deleteBlog, updateBlog } = require('../controller/blogController');
+const { storeBlog, getBlog, updateBlog, deleteBlog } = require('../controller/blogController');
 
 // Define a rota POST para armazenar um novo blog. Quando um pedido POST é feito para '/store/blog', a função storeBlog é chamada
+/**
+ * @swagger
+ * /store/blog:
+ *   post:
+ *     summary: Armazena um novo blog
+ *     responses: 
+ *       200:
+ *         description: Post do blog
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ */
 router.post('/store/blog', storeBlog);
 
 // Define a rota GET para obter todos os blogs. Quando um pedido GET é feito para '/get/blog', a função getBlog é chamada
+/**
+ * @swagger
+ * /get/blog:
+ *   get:
+ *     summary: Define a rota GET para obter todos os blogs
+ *     responses: 
+ *       200:
+ *         description: Post do blog
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ */
 router.get('/get/blog', getBlog);
 
-// Define a rota DELETE para deletar um blog específico. Quando um pedido DELETE é feito para '/delete/blog/:id', a função deleteBlog é chamada
-router.delete('/delete/blog/:id', deleteBlog);
-
 // Define a rota PUT para atualizar um blog específico. Quando um pedido PUT é feito para '/update/blog/:id', a função updateBlog é chamada
+/**
+ * @swagger
+ * /update/blog/:id:
+ *   put:
+ *     summary: Define a rota PUT para atualizar um blog específico
+ *     responses: 
+ *       200:
+ *         description: Post do blog
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ */
 router.put('/update/blog/:id', updateBlog);
+
+// Define a rota DELETE para deletar um blog específico. Quando um pedido DELETE é feito para '/delete/blog/:id', a função deleteBlog é chamada
+/**
+ * @swagger
+ * /delete/blog/:id:
+ *   delete:
+ *     summary: Define a rota DELETE para deletar um blog específico
+ *     responses: 
+ *       200:
+ *         description: Post do blog
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ */
+router.delete('/delete/blog/:id', deleteBlog);
 
 // Exporta o roteador para que possa ser usado em outros arquivos do projeto
 module.exports = router;
--- api/src/server.js
+++ api/src/server.js
@@ -1,9 +1,28 @@
 // Inicia o servidor.
+const swaggerUi = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API de Tarefas",
+            version: "1.0.0",
+            description: "API CRUD para gerenciar tarefas",
+        },
+        servers: [{ url: "http://localhost:3003" }],
+    },
+    apis: [`${__dirname}/routes/*.js`], // caminho para as rotas
+};
+
+const taskRouter = require('./routes/blogRouter.js');
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 
 // Importar o arquivo app
 const app = require('./app');
 // Importar a porta do servidor
 const port = app.get('port');
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // Testar API
 app.listen(port, () => console.log(`Run on port ${port}!`));
