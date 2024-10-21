1c8d698ea6e312f968a9b84e396f8f9ee7b43973
Lívia Escobar <liviaescobar06@gmail.com>
2024-10-21 09:57:04 -0300

    info inicio

---
+++ front/scriptInfo.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.painel-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const accordionContent = button.nextElementSibling;
+
+        button.classList.toggle('active');
+
+        if (button.classList.contains('active')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+        } else {
+            accordionContent.style.maxHeight = 0;
+        }
+    });
+});
